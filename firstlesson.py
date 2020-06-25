426fab72115fb9f4b1cb9b591ccbb75606cf5a3e
ethanglaser <glasere@purdue.edu>
2020-06-25 12:09:58 -0500

    first lesson complete

---
+++ firstlesson.py
@@ -0,0 +1,40 @@
+
+def function():
+    print("Hello, World!")
+
+def initialize():
+    variable = 1
+    variableString = "Ethan"
+    print(variable)
+    print(variableString)
+
+def domath():
+    a = 2 + 3
+    print(a)
+
+
+    sub = 10 - 5
+    mul = 6 * 3
+    div = 9 / 3
+
+    squaredvalue = 15 ** 2
+    print(squaredvalue)
+
+    cubed = 3 ** 3
+    print(cubed)
+
+    mod = 11 % 4
+    print(mod)
+    
+
+
+if __name__ == "__main__":
+    #this function prints hello world
+    '''
+    multi
+    line
+    comment
+    '''
+    function()
+    initialize()
+    domath()
