47af1a3f33298c86d52c8e07eb1fa7356d4d91e9
ethanglaser <glasere@purdue.edu>
2020-06-25 14:56:32 -0500

    sample problems from first lesson

---
+++ review.py
@@ -0,0 +1,55 @@
+
+'''
+To review what we covered today, I created a Python file with some prompts for functions to write (I wrote two of them as an example)
+If you run this file without modifying it, 2 lines should be printed in the terminal:
+
+Code is running
+12
+
+
+Reminders:
+Variable can be named whatever you want, but once they are named they can only be referenced by the name you originally gave them
+The same applies for function names - you can name them anything
+Call the functions from main to run them
+Remember that functions are declared using "def" and require parentheses and a colon
+The code inside a function should be tabbed over
+Setting a variable requires a single equals-sign and the variable name should be on the left with its value on the right of the =
+'''
+
+
+#Write and call a function from main called "sample" that prints that the code is running
+def sample():
+    print("Code is running")
+
+
+#Write a function that prints "Hi my name is ___" with your name in the blank
+
+
+#Write a function that prints the grade you are going into and the school you go to on separate lines
+
+
+#Write a function that sets a variable to your favorite number and prints it
+def favoritenumber():
+    favorite = 12
+    print(favorite)
+
+
+#Write a function that finds the sum of two numbers (you can pick the numbers) and prints that sum
+
+
+#Write a function that prints that sets variables for two numbers, then print the product of those numbers
+
+
+#Write a function that prints the adds, subtracts, multiplies, and divides 6 and 3 and print the 4 results on separate lines
+
+
+#Write a function that prints the result of 5^2 and 6^3 (remember that ^ is not the python command for exponents)
+
+
+#Write a function that sets variables to 2 integers and then find the remainder when the first number is divided by the second number
+
+
+
+if __name__ == "__main__":
+    sample()
+    favoritenumber()
