8696588896c397291022e82c1291764d0fb1682d
ethanglaser <glasere@purdue.edu>
2020-07-17 23:18:00 -0500

    review 4 added

---
+++ Review/review4.py
@@ -0,0 +1,16 @@
+#Write a function that takes in 1 integer and returns either 1, 2, 3, or 4:
+# 1 if the number is divisible by both 2 and 3 (for example, 12)
+# 2 if the number is divisible by just 2 and not 3 (for example, 4)
+# 3 if the number is divisible by just 3 and not 2 (for exmaple, 15)
+# 4 if the number is divisible by neither 2 nor 3 (for example, 11)
+
+
+#Write a function that takes in 2 variables - the first is the type of shape (1 for square, 2 for circle)
+# and the second is the length (side length if square and radius if circle), return the area of the shape (use 3.14 for pi)
+# Example 1: function(1, 5) should return 25 because it is a square and side length is 5 so area is 25
+# Example 2: function(2, 10) should return 314 because it is a circle and 3.14 * 10 * 10 = 314
+
+
+
+if __name__ == "__main__":
+
