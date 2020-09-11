ff237783b5213396fe1e605f7277632b6a0589fd
ethanglaser <glasere@purdue.edu>
2020-09-11 17:36:24 -0400

    lesson 12 and review

---
+++ Code from lessons/lesson12.py
@@ -0,0 +1,33 @@
+
+def firstfunction(filename):
+    with open('Data Files/' + filename, 'r') as f:
+        data = f.readlines()
+    return data
+    '''if numbers == False:
+        return data
+
+    
+    intdata = []
+    for line in data:
+        intdata.append(int(line))
+    return intdata
+
+
+def buildings():
+    buildingsData = firstfunction('buildings10.txt', True)
+    print(max(buildingsData))'''
+
+def longword():
+    words = firstfunction('manyWords.txt')
+    leng = 0
+    for word in words:
+        if len(word) > leng:
+            leng = len(word)
+            big = word
+    print(big)
+
+if __name__ == "__main__":
+    #print(firstfunction('sample.txt', True))
+    #buildings()
+    longword()
+
---
+++ Review/review10.py
@@ -0,0 +1,11 @@
+## All problems here will involve manyWords.txt (or whatever you named the 10000 word file)
+
+# Write out a function that prints out the number of words in the file that are over 10 letters long
+
+
+# Write a function to determine whether the letter q, x, or z appears least in the entire file
+
+
+# Write a function that takes in a letter and prints out the number of words in the file that start with that letter
+# example: function('a') would print 720
+
