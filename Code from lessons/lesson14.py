db7723000465192528d2bd64c641cb116c8106df
ethanglaser <ethanglaser12@gmail.com>
2020-11-22 15:14:36 -0500

    lesson 14 complete, project document created

--- Code from lessons/lesson14.py
+++ Code from lessons/lesson14.py
@@ -13,18 +13,121 @@ N-grams
 '''
 
 #print all numbers less than 100 that are divisible by 7
+def loop():
+    for i in range(100):
+        if i % 7 == 0:
+            print(i)
 
 #print numbers from a list that are even and greater than 10
+def printlist(mylist):
+    for value in mylist:
+        if value > 10 and value % 2 == 0:
+            print(value)
 
 #take a string and print how many times a letter appears
+def stringfunction(mystring, letter):
+    #print(mystring.count(letter))
+    count = 0
+    for char in mystring:
+        if char == letter:
+            count += 1
+    print(count)
 
 #take a sentence and determine how many words are in it
+def countwords(val):
+    wordlist = val.split()
+    print(len(wordlist))
 
 #find the length of the longest word in a sentence
+def findlong(sentence):
+    wordlist = sentence.split()
+    maxlength = 0
+    for word in wordlist:
+        wordlength = len(word)
+        if wordlength > maxlength:
+            maxlength = wordlength
+            maxword = word
+    print(maxlength, maxword)
+
+#read in a file and determine how many words are in it
+def numwords():
+    with open("spanish.txt", "r") as f:
+        lines = f.readlines()
+    count = 0
+    for line in lines:
+        words = line.split()
+        numwords = len(words)
+        count += numwords
+    print(count)
+
+def liststuff():
+    new = [1,2,3,4,5,6,9,1,3]
+    print(new[0])
+
+    mylist = []
+    mylist.append(5)
+
+    points = [[100, 7], [20, 500]]
+    pointsdict = {}
+    vikings = {}
+    vikings['pf'] = 100
+    vikings['pa'] = 7
+    print(vikings)
+    packers = {}
+    packers['pf'] = 20
+    packers['pa'] = 500
+
+    pointsdict['Vikings'] = vikings
+    pointsdict['Packers'] = packers
+
+    print(pointsdict)
+
+
+    spanish = {}
+    spanish['el '] = 100
+    spanish['los'] = 82
+    spanish['ero'] = 29
+
+
+
+
+if __name__ == "__main__":
+    loop()
+    newlist = [1,3,9,12,25,42]
+    printlist(newlist)
+    mystring = 'mississippi'
+    letter = 'i'
+    stringfunction(mystring, letter)
+    sentence = "Hi my name is Ethan"
+    countwords(sentence)
+    findlong(sentence)
+    numwords()
+    liststuff()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #take a list of integers and number, add that number of numbers together to form a new list
 
-#read in a file and determine how many words are in it
+
 
 '''
 Dictionaries:
