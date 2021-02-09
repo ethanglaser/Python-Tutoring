72601489a2928723a178da899e97592faa708623
ethanglaser <glasere@purdue.edu>
2021-02-08 20:33:10 -0500

    lesson 19 added

---
+++ Code from lessons/lesson19.py
@@ -0,0 +1,25 @@
+'''
+QUIZ
+
+1. Which of the following commands is not used when moving code changes from your local folder to your online GitHub repository?
+ A. push
+ B. commit
+ C. clone
+ D. add
+ E. All of the above are used every time you update your repository
+
+2. Which of the following would you use 'git clone' for?
+ A. You just created a new repository on GitHub and want to create a corresponding directory locally on your laptop.
+ B. You found a cool project on GitHub and want to play around with the code on your device.
+ C. You have been working on a project and want to duplicate it to email it to your friend to work on.
+ D. A and B
+ E. A and C
+
+'''
+
+#Review Git - clone, add, commit, push
+#branches - get usernames so they can edit tictactoe repo
+
+
+
+
